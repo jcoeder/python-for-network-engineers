a14a8082f1c9ecaf5dc2a4df848a20e2382f685f
jcoeder <jcoeder@gmail.com>
2018-10-21 11:04:02 -0400

    A

--- week4/python_expect.py
+++ week4/python_expect.py
@@ -2,10 +2,10 @@
 
 import pexpect
 import sys
-
+import re
 
 def main():
-    ip_addr = '172.31.33.80'
+    ip_addr = '172.31.33.81'
     username = 'admin'
     password = 'admin'
     port = 22
@@ -15,29 +15,51 @@ def main():
     #Log stuff to stdout
     ssh_conn.logfile = sys.stdout
     #Set SSH timeout
-    ssh_conn.timout = 3
+    ssh_conn.timeout = 3
 
     ssh_conn.expect('ssword:')
     ssh_conn.sendline(password)
 
-    ssh_conn.expect('#')
-    print (ssh_conn.before)
+    ssh_conn.expect('router#')
+    #print (ssh_conn.before)
 
     def send_command(cmd, expect):
-        ssh_conn.sendline(cmd)
-        ssh_conn.expect('router#')
+        '''
+       Sends command and if the expect is not found
+       print PyExpect has timed out.
+       '''
+        try:
+            ssh_conn.sendline(cmd + '\n')
+            ssh_conn.expect(expect)
+            #print(ssh_conn.before)
+            #print(ssh_conn.after)
+        except pexpect.TIMEOUT:
+            print ('PyExpect has timed out')
 
     send_command('terminal length 0', 'router#')
-    print ('')
-    send_command('show ip interface brief', 'router#')
-    print ('')
-    send_command('show version', 'router#')
+    #send_command('show ip interface brief', 'router#')
+    #send_command('show version', 'router#')
+    #This will timeout and meet the except statement
+    #send_command('show version', 'YOLO')
 
-    try:
-        ssh_conn.sendline('show version')
-        ssh_conn.expect('WILL NOT SHOW UP')        
-    except pexpect.TIMEOUT:
-        print 'Found timeout'
+    #Regular Expressions should always be raw 'r' strings
+    #Begins with '^' Lic
+    #Any squence of 0 or more caracters endings in DI:
+    #Any squence of 0 or more caracters end of the line
+    #Multiline - Line by line match
+    #pattern = re.compile(r'^Lic.*DI:.*$', re.MULTILINE)
+    #ssh_conn.sendline('show version')
+    #ssh_conn.expect(pattern)
+
+
+    #Regular Expressions should always be raw 'r' strings
+    #Begins with '^' ROM
+    #Any squence of 0 or more caracters end of the line
+    #Multiline - Line by line match
+    pattern = re.compile(r'^ROM:.*$', re.MULTILINE)
+    ssh_conn.sendline('show version')
+    ssh_conn.expect(pattern)
+    print (ssh_conn.readline())
 
 if __name__ == "__main__":
     main()
