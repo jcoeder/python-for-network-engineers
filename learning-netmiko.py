f9e7faac84d72ed9e3aed46f36116c0233437545
Justin Oeder <jcoeder@gmail.com>
2019-07-12 09:59:59 -0400

    Create learning-netmiko.py

---
+++ learning-netmiko.py
@@ -0,0 +1,16 @@
+from netmiko import ConnectHandler
+
+net_connect = ConnectHandler(host='iol1', username='justin', password='Lab12345!', device_type='cisco_ios')
+#or
+device1 = {
+    'host': 'iol1',
+    'username': 'justin',
+    'password': 'Lab12345!',
+    'device_type': 'cisco_ios',
+    'session_log': 'iol1.log',  # can log session to file
+}
+
+net_connect = ConnectHandler(**device1)
+
+print(net_connect.send_command('show version'))
+print(net_connect.send_command('show ip inter br'))
